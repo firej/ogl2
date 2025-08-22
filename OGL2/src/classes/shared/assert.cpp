34a2a374e93016051de625e4aae5d4fca57c01cc
Evgenii Bogdanov <firej@ya.ru>
2025-08-22 21:55:17 +0300

    permissions?

--- OGL2/src/classes/shared/assert.cpp
+++ OGL2/src/classes/shared/assert.cpp
@@ -1,4 +1,3 @@
-#include "LocusAFX.h"
 #include "./ASSERT.h"
 
 #pragma warning(disable:4996)
@@ -7,16 +6,16 @@ char  __stdcall _FASSERT(void* expr, void *filename, unsigned int linenum,const 
 {
 	if (!expr)
 	{
-		ShowCursor(true);
+//		ShowCursor(true);
         char buf[400] = "";
 		sprintf(buf,"Не выполняется утверждение в программе!\nУтверждение:%s\nФайл:\"%s\"\nСтрока №%u\n\tПрервать - выход\n\tПовторить - перейти в Debug\n\tОтменить - игнорировать",FunctionName,filename,linenum);
-		int ret = MessageBox(0,buf,MY_APP_ERROR_HDR,MB_ABORTRETRYIGNORE | MB_ICONWARNING | MB_DEFBUTTON2);
-		switch(ret)
-		{
-		case IDABORT	:ExitProcess(0)		;break;
-		case IDRETRY	:return NEED_DEBUG	;break;									// DEBUG останов
-		case IDIGNORE	:ShowCursor(false); return NEED_IGNORE	; break;	// Игнор
-		}
+//		int ret = MessageBox(0, buf, MY_APP_ERROR_HDR, MB_ABORTRETRYIGNORE | MB_ICONWARNING | MB_DEFBUTTON2);
+//		switch(ret)
+//		{
+//		case IDABORT	:ExitProcess(0)		;break;
+//		case IDRETRY	:return NEED_DEBUG	;break;									// DEBUG останов
+//		case IDIGNORE	:ShowCursor(false); return NEED_IGNORE	; break;	// Игнор
+//		}
 	}
 	return 0;
 }
